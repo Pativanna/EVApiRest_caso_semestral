bd5188ca3bb1ff1548844c9ff3a2cf20d6d7bace
Pativanna <seb.nunez@duocuc.cl>
2023-10-18 21:59:10 -0300

    1234
    1234

--- django/apichorisima/core/views.py
+++ django/apichorisima/core/views.py
@@ -1,19 +1,26 @@
-from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
-
-from rest_framework import viewsets
 from .serializer import UserSerializer
 from .models import user
 
-
-# Create your views here.
-@csrf_exempt
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def lista_usuarios(request):
-    User = user.objects.all()
-    serializer = UserSerializer(User, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        users = user.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        data = request.data
+        correo = data.get('correo')
+
+        # Verificar si ya existe un usuario con el mismo correo electrónico
+        existing_user = user.objects.filter(correo=correo).first()
+        if existing_user:
+            return Response({'error': 'Ya existe un usuario con este correo electrónico.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = UserSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

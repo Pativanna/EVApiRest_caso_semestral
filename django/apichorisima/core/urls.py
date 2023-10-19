3686fd033a3008ca1836aea9ba082172eb5b8809
Pativanna <seb.nunez@duocuc.cl>
2023-10-19 14:23:50 -0300

    Update urls.py

--- django/apichorisima/core/urls.py
+++ django/apichorisima/core/urls.py
@@ -4,4 +4,5 @@ from core.views import lista_usuarios, autenticar_usuario
 urlpatterns=[
     path('usuarios/', lista_usuarios, name='lista_usuarios'),
     path('autenticar/', autenticar_usuario, name='autenticar_usuario'),
+    #123
 ]
